cee66006ac16deea6f84fc8ea5519c8f35a0d060
Kaya Juwelier <serka@kayajuwelier.dev>
2026-05-13 17:11:15 +0300

    Fix coin weights: Çeyrek 1.75g, Yarım 3.5g, Tam 7g, Gremse 17.5g, Beşli 35g

--- backend/KayaJuwelier.GoldPrice.Api/wwwroot/flutter_bootstrap.js
+++ backend/KayaJuwelier.GoldPrice.Api/wwwroot/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2126110306" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4283011828" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
